e0719a5151ae1db0e0c2a3bbcb6fa0b356c01706
Vaibhav <vk10decvaibhav@gmail.com>
2021-02-03 01:04:26 +0530

    Completed

--- script.js
+++ script.js
@@ -8,16 +8,31 @@ document.querySelector(".H").onclick = function() {
     window.scroll(0, 0);
 };
 
+document.querySelector(".A").onclick = scrollToAbout;
 document.querySelector(".D").onclick = scrollToWebDesigns;
 
 document.querySelector(".P").onclick = scrollToWebDevProjects;
 
-function scrollToWebDesigns() {
+function scrollToAbout() {
     var scrollDistance = showcase.offsetHeight - window.scrollY;
     window.scrollBy(0, scrollDistance);
 };
 
+function scrollToWebDesigns() {
+    var scrollDistance = showcase.offsetHeight - window.scrollY;
+
+    if (screen.width <= 1436) {
+        window.scrollBy(0, scrollDistance + document.querySelector(".Side-Bar").offsetHeight);
+    } else {
+        window.scrollBy(0, scrollDistance);
+    }
+};
+
 function scrollToWebDevProjects() {
     var scrollDistance = document.getElementById("First-Block").offsetHeight + showcase.offsetHeight - window.scrollY + 10; // Extra 10px to compensate for uneven margins caused due to texts
-    window.scrollBy(0, scrollDistance);
+    if (screen.width <= 1436) {
+        window.scrollBy(0, scrollDistance + document.querySelector(".Side-Bar").offsetHeight);
+    } else {
+        window.scrollBy(0, scrollDistance);
+    }
 };
